2d71b1e9de7356c9aa20db1e6213e890d6f51c52
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-10-18 23:44:06 -0300

    rotas estabelecidas e endpoint selecionado

---
+++ pokedex/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './Home/Home'
+import Detalhes from './Detalhes/Detalhes'
+import Pokedex from './Pokedex/Pokedex'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        
+        <Route exact path={"/"}>
+          <Home/>
+        </Route>
+
+        <Route exact path={"/Pokedex"}>
+          <Pokedex/>
+        </Route>
+
+        <Route exact path={"/Detalhes"}>
+          <Detalhes/>
+        </Route>
+        
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
