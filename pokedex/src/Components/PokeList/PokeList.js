e9a38b2386de5c6d337592b58d364b3829e03306
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-10-22 10:53:20 -0300

    Cards renderizados

--- pokedex/src/Components/PokeList/PokeList.js
+++ pokedex/src/Components/PokeList/PokeList.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {useEffect, useState} from 'react'
 import axios from 'axios'
+import './PokeList.css'
 
 function PokeList (props) {
+
+  // props é um objeto com os nomes
 
     const [pokemons, setPokemons] = useState()
 
@@ -10,20 +13,22 @@ function PokeList (props) {
         const url = `https://pokeapi.co/api/v2/pokemon/${props.nome}/`
         axios.get(url)
         .then((response) => {
-        //   console.log(typeof(response.data))
           setPokemons(response.data)
-        //   console.log("DENTRO DA FUNÇÃO", pokeList)
-          // console.log("LISTA: ", typeof(pokeList))
         }).catch((error) => {
           console.log(error)
         })
     },[])
 
-    console.log(pokemons)
+    const addPokedex = () => {
+
+    }
 
     return(
-        <div>
+        <div className='card'>
+            {pokemons !== undefined && <p>{pokemons.name}</p>}
             {pokemons !== undefined && <img src={pokemons.sprites.front_default}/>}
+            <button onClick={() => addPokedex}>Adicionar</button>
+            <button>Detalhes</button>
         </div>
     )
 }
